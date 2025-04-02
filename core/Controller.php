b13ef268b6b9b6385e5952c986e651bc042bad9f
Mathias Celle <[mathias.celle42230@gmail.com]>
2025-04-02 11:25:00 +0200

    config/database.php

---
+++ core/Controller.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\core;
+
+class Controller
+{
+    protected function render(string $view, array $data = [])
+    {
+        extract($data); // Extrait les clés du tableau $data en tant que variables
+        require ROOT . '/views/' . $view . '.php';
+    }
+}
